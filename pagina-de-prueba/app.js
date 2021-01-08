d1bb0f85006b3562ac12449ca858c91485e58e00
roberts6 <roberts_oscar@hotmail.com>
2021-01-08 15:38:57 -0600

    no puedo centrar la clase boton

--- pagina-de-prueba/app.js
+++ pagina-de-prueba/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use('/static', express.static(__dirname + '/public'));
+
 const publicPath = path.resolve(__dirname, "./practicasVarias");
 
-app.listen(3666, () => console.log("Funcionando en el puerto 3666"));
+app.listen(3506, () => console.log("Funcionando en el puerto 3506"));
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "views/home.html")
